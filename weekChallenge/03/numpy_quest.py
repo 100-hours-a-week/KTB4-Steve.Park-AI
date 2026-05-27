46d029cd87dd9f5f3312612fcf6670b9c91dc292
Loa Park <1sanguk@naver.com>
2026-05-27 13:11:49 +0900

    Update numpy_quest.py
    변경된 Numpy의 Shape문제를 다시 품

--- weekChallenge/03/numpy_quest.py
+++ weekChallenge/03/numpy_quest.py
@@ -44,11 +44,7 @@ array = array.reshape(2,3)
 printanswer(2, array)
 
 ## 3
-array = np.array([
-    [[1,2], [3,4]],
-    [[5,6], [7,8]],
-    [[9,10], [11,12]]
-])
+array = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
 
 array = array.reshape(3, 2, 2)
 
